0dd4e683a5dacf7eb773d8a395d33c0947a09fd5
Minlong Sung <asungyang@gmail.com>
2024-03-12 23:33:06 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, splitVendorChunk, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+
+export default defineConfig({
+  // base: './', // when using github pages to deploy, so that detects root url
+  envPrefix: 'PUBLIC_',
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@emotion'],
+      },
+    }),
+    splitVendorChunkPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
